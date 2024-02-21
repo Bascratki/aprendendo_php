1b57862d91478f5b81e420620e2a6b1bdde738e1
Go Ingressos <160746304+Bascratk1@users.noreply.github.com>
2024-02-21 13:27:59 -0300

    Update banco.php

--- primeiros_passos/Avancando/banco.php
+++ primeiros_passos/Avancando/banco.php
@@ -1,6 +1,6 @@
 <?php
 
-function sacar($conta, $valorASacar)
+function sacar(array $conta, float $valorASacar): array
 {
     if ($valorASacar > $conta['saldo']) {
         exibeMensagem("Você não pode sacar esse valor");
@@ -10,7 +10,17 @@ function sacar($conta, $valorASacar)
     return $conta;
 }
 
-function exibeMensagem ($mensagem) 
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depósitos precisam ser positivos");
+    }
+    return $conta;
+}
+
+function exibeMensagem (string $mensagem) 
 {
     echo $mensagem . PHP_EOL;
 }
@@ -33,7 +43,9 @@ $contasCorrentes = [
 $contasCorrentes ['123.456.789-10'] = sacar($contasCorrentes ['123.456.789-10'], 500);
 $contasCorrentes ['123.456.689-11'] = sacar($contasCorrentes ['123.456.689-11'], 500);
 
+$contasCorrentes ['123.256.789-12'] = depositar($contasCorrentes ['123.256.789-12'], 900);
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . " " . $conta['saldo']);
+    exibeMensagem ("A conta <$cpf> de {$conta['titular']} possui R$$conta[saldo] de saldo");
 }
  
