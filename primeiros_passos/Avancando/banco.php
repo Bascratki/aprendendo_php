b6913e29404a8750b4b24ee9cf530af0bc07b7a1
Go Ingressos <160746304+Bascratk1@users.noreply.github.com>
2024-02-21 17:03:38 -0300

    Edições

--- primeiros_passos/Avancando/banco.php
+++ primeiros_passos/Avancando/banco.php
@@ -18,15 +18,36 @@ $contasCorrentes = [
 ];
 
 $contasCorrentes ['123.456.789-10'] = sacar($contasCorrentes ['123.456.789-10'], 500);
-$contasCorrentes ['123.456.689-11'] = sacar($contasCorrentes ['123.456.689-11'], 500);
+$contasCorrentes ['123.456.689-11'] = sacar($contasCorrentes ['123.456.689-11'], 200);
 
 $contasCorrentes ['123.256.789-12'] = depositar($contasCorrentes ['123.256.789-12'], 900);
 
 unset($contasCorrentes['123.456.689-11']);
 
 nomeMaiusculo ($contasCorrentes['123.256.789-12']);
+?>
 
-foreach ($contasCorrentes as $cpf => $conta) {
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    exibeMensagem ("A conta de $titular com o CPF: $cpf, possui R$$saldo de saldo.");
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?php echo $conta['titular']; ?> - <?php echo $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?php echo $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dt>
+</body>
+</html>
--- primeiros_passos/Avancando/funcoes.php
+++ primeiros_passos/Avancando/funcoes.php
@@ -1,31 +1,37 @@
 <?php
 
-function sacar(array $conta, float $valorASacar): array
+function nomeMaiusculo (array &$conta)
+{
+    $conta['titular'] = mb_strtoupper($conta['titular']);
+}
+
+function exibeMensagem (string $mensagem) 
+{
+    echo $mensagem . '<br>';
+}
+
+function exibeConta (array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular. Saldo: $saldo</li>";
+}
+
+function sacar (array $conta, float $valorASacar): array
 {
     if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar esse valor");
+        exibeMensagem ("Você não pode sacar esse valor");
     } else {
         $conta['saldo'] -= $valorASacar;
     }
     return $conta;
 }
 
-function depositar(array $conta, float $valorADepositar): array
+function depositar (array $conta, float $valorADepositar): array
 {
     if ($valorADepositar > 0) {
         $conta['saldo'] += $valorADepositar;
     } else {
-        exibeMensagem("Depósitos precisam ser positivos");
+        exibeMensagem ("Depósitos precisam ser positivos");
     }
     return $conta;
-}
-
-function exibeMensagem (string $mensagem) 
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function nomeMaiusculo (array &$conta)
-{
-    $conta['titular'] = mb_strtoupper($conta['titular']);
 }
