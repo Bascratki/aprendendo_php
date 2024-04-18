4c291c5952251cef5454c106daeb364d6a22968d
Bascratki <lucasstopinskidasilva@gmail.com>
2024-04-18 18:09:04 -0300

    Update background

--- next.js/projeto_completo/components/welcome/index.jsx
+++ next.js/projeto_completo/components/welcome/index.jsx
@@ -5,25 +5,26 @@ import Image from 'next/image'
 
 const Welcome = () => {
 	return (
-		<div className={Styles['luz-fundo']}>
-			<div className={Styles.container}>
-				<div className={Styles.text}>
-					<h1>Melhor agência de marketingdo bairro</h1>
-					<p>
-						Somos umas agência de marketing digital focada em resultados.
-						Ajudamos empresas a crescerem na internet.
-					</p>
-					<Button
-						title='Aumentar vendas'
-						kind='secundary'
-					/>
-				</div>
-				<div className={Styles.image}>
-					<Image
-						src={BannerWelcome}
-						alt='Banner Welcome'
-					/>
-				</div>
+		<div className={Styles.container}>
+			<div className={Styles.text}>
+				<h1>Melhor agência de marketingdo bairro</h1>
+				<p>
+					Somos umas agência de marketing digital focada em resultados. Ajudamos
+					empresas a crescerem na internet.
+				</p>
+				<Button
+					title='Aumentar vendas'
+					kind='secundary'
+				/>
+			</div>
+			<div className={Styles.image}>
+				<Image
+					src={BannerWelcome}
+					alt='Banner Welcome'
+				/>
+			</div>
+			<div className={Styles.animation}>
+				{/* Add your animation component here */}
 			</div>
 		</div>
 	)
