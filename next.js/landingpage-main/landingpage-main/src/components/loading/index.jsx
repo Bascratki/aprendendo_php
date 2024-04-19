650b41d28f8e175351e761bc2fb2d1c871905833
Lucas Stopinski da Silva <160746304+Bascratki@users.noreply.github.com>
2024-04-19 14:04:21 -0300

    Organização

--- next.js/landingpage-main/landingpage-main/src/components/loading/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./styles.module.scss";
-
-export const Loading = () => (
-  <div className={styles.overlay}>
-    <div className={styles.loading} />
-  </div>
-);
--- next.js/landingpage-main/landingpage-main/src/utils/scrollTo.js
+++
@@ -1,4 +0,0 @@
-export const ScrollTo = (id) => {
-  const elemento = document?.getElementById(id);
-  elemento?.scrollIntoView();
-};
