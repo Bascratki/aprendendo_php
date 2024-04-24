892129dca3cf1ad681438b8e172321548b6c82e3
Lucas Stopinski da Silva <160746304+Bascratki@users.noreply.github.com>
2024-04-24 16:49:06 -0300

    Iniciando footer

---
+++ next.js/projeto_completo/components/footer/index.jsx
@@ -0,0 +1,16 @@
+import Styles from './footer.module.scss'
+
+const Footer = () => {
+	return (
+		<div className={StyleSheet.container}>
+			<div className={Styles.column}>
+                <Image src={}
+            </div>
+			<div className={Styles.column}></div>
+			<div className={Styles.column}></div>
+			<div className={Styles.column}></div>
+		</div>
+	)
+}
+
+export default Footer
